5af673eb0093fce4e90ea2712f99edf957a7f004
Artenni <artenni4@gmail.com>
2021-05-30 14:23:30 +0300

    add target for file scanning

--- test/test_file.cpp
+++ test/test_file.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "Simplexer.hpp"
+
+
+void printTokens(const std::vector<Simplexer::Token>& ts) {
+    for (auto iter = ts.begin(); iter != ts.end(); ++iter) {
+        std::stringstream buff;
+
+        // line
+        buff << "Line: " << iter->line;
+        while (buff.str().size() < 15) {
+            buff << ' ';
+        }
+
+        // type
+        buff << " Type: " << Simplexer::PRINTABLE_TYPES[static_cast<int>(iter->type)];
+        while (buff.str().size() < 35) {
+            buff << ' ';
+        }
+
+        // symbol
+        buff << " Symbol: " << iter->symbol;
+
+        std::cout << buff.str() << std::endl;
+    }
+}
 
 int main(int argc, char** argv) {
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
+        exit(-1);
+	}
+	
+	Simplexer::Lexer lex;
+	// try to read file
+	if (lex.readFile(argv[1])) { // on success
+        std::vector<Simplexer::Token> tokens;
+        for (auto T = lex.next(); T.type != Simplexer::TokenType::END_OF_FILE; T = lex.next()) {
+            tokens.emplace_back(T);
+        }
+        printTokens(tokens);
+	}
+	else { // on failure
+		std::cout << "Failed to read file. Exiting..." << std::endl;
+	}
 
 	return 0;
 }
