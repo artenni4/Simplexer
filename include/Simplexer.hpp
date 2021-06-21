48b18b26bd21a3a740a8ab168e155c024893dc57
Artenni <artenni4@gmail.com>
2021-06-21 11:38:16 +0300

    make processing token in Lexer accessible for entire class

--- include/Simplexer.hpp
+++ include/Simplexer.hpp
@@ -101,12 +101,13 @@ namespace Simplexer {
     class Lexer {
     private:
         std::string m_rawString; // string to analyze
+        mutable Token m_currToken; // current Token to return 
         mutable std::string::const_iterator m_pos; // current lexer position in string
 
     private:
 
         // return string with symbol's line and row
-        void Lexer::getSymbolLine(Token& tk) const {
+        void Lexer::getSymbolLine() const {
             std::size_t row = 0, line = 1; // start line from 1, not from 0
             std::string::const_iterator lastNl = m_rawString.begin(); // last new line
 
@@ -118,44 +119,44 @@ namespace Simplexer {
             }
             row = m_pos - lastNl + 1; // shift one char to right, so first is 1 but not 0
 
-            tk.symbolLine = line;
-            tk.symbolRow = row;
+            m_currToken.symbolLine = line;
+            m_currToken.symbolRow = row;
         }
 
         // get symbol from current position
-        void Lexer::parseSymbol(Token& tk) const
+        void Lexer::parseSymbol() const
         {
-            tk.type = TokenType::SYMBOL;
+            m_currToken.type = TokenType::SYMBOL;
             // fill in string for token
             while (std::isalpha(*m_pos) || std::isdigit(*m_pos) || *m_pos == '_') {
-                tk.symbol += *m_pos++;
+                m_currToken.symbol += *m_pos++;
             }
             --m_pos; // return to the last character
         }
         // get number from current position
-        void Lexer::parseNumber(Token& tk) const
+        void Lexer::parseNumber() const
         {
             // get all digits and skip if point is on the first place
             while (std::isdigit(*m_pos)) {
-                tk.symbol += *m_pos++;
+                m_currToken.symbol += *m_pos++;
             }
 
             // check for point after previous loop
             if (*m_pos == '.') { // add point to symbol string and define type as RATIONAL
-                tk.symbol += *m_pos++;
+                m_currToken.symbol += *m_pos++;
                 while (std::isdigit(*m_pos)) {
-                    tk.symbol += *m_pos++;
+                    m_currToken.symbol += *m_pos++;
                 }
-                tk.type = TokenType::RATIONAL;
+                m_currToken.type = TokenType::RATIONAL;
             }
             else { // if there is no point then type is INTEGER
-                tk.type = TokenType::INTEGER;
+                m_currToken.type = TokenType::INTEGER;
             }
             --m_pos; // return to the last character
         }
         // get string from current position
-        void Lexer::parseString(Token& tk) const {
-            tk.type = TokenType::STRING;
+        void Lexer::parseString() const {
+            m_currToken.type = TokenType::STRING;
             // remember starting quote and fill string to same quote
             char quote = *m_pos++;
 
@@ -171,114 +172,114 @@ namespace Simplexer {
                     // if current position is end of file or new line
                     // this means that we have overhead of the string on one charcter
                     // so go back for one position
-                    tk.type = TokenType::INVALID;
+                    m_currToken.type = TokenType::INVALID;
                     break;
                 }
                 else if (*m_pos == '\\') { // if encounter backslash
                     if (*(m_pos + 1) == quote) { // and next char is quote parseString started with
                         // then swallow this quote
-                        tk.symbol += *++m_pos;
+                        m_currToken.symbol += *++m_pos;
                         ++m_pos; // skip the quote
                     }
                     else {
-                        tk.symbol += *m_pos++;
+                        m_currToken.symbol += *m_pos++;
                     }
                 }
                 else {
-                    tk.symbol += *m_pos++;
+                    m_currToken.symbol += *m_pos++;
                 }
             }
         }
         // processes all signs, use to get any combination for token type
-        void Lexer::parseOperator(Token& tk) const {
+        void Lexer::parseOperator() const {
             auto next = m_pos + 1; // store next character after current position
             switch (*m_pos) {
             case '+':
                 if (*next == '+') {
-                    tk.type = TokenType::PLUS_PLUS;
+                    m_currToken.type = TokenType::PLUS_PLUS;
                     ++m_pos;
                 }
                 else if (*next == '=') {
-                    tk.type = TokenType::PLUS_EQUAL;
+                    m_currToken.type = TokenType::PLUS_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::PLUS;
+                    m_currToken.type = TokenType::PLUS;
                 }
                 break;
             case '-':
                 if (*next == '-') {
-                    tk.type = TokenType::MINUS_MINUS;
+                    m_currToken.type = TokenType::MINUS_MINUS;
                     ++m_pos;
                 }
                 else if (*next == '=') {
-                    tk.type = TokenType::MINUS_EQUAL;
+                    m_currToken.type = TokenType::MINUS_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::MINUS;
+                    m_currToken.type = TokenType::MINUS;
                 }
                 break;
             case '*':
                 if (*next == '=') {
-                    tk.type = TokenType::ASTERISK_EQUAL;
+                    m_currToken.type = TokenType::ASTERISK_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::ASTERISK;
+                    m_currToken.type = TokenType::ASTERISK;
                 }
                 break;
             case '/':
                 if (*next == '=') {
-                    tk.type = TokenType::SLASH_EQUAL;
+                    m_currToken.type = TokenType::SLASH_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::SLASH;
+                    m_currToken.type = TokenType::SLASH;
                 }
                 break;
             case '=':
                 if (*next == '=') {
-                    tk.type = TokenType::DOUBLE_EQUAL;
+                    m_currToken.type = TokenType::DOUBLE_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::EQUAL;
+                    m_currToken.type = TokenType::EQUAL;
                 }
                 break;
             case '<':
                 if (*next == '=') {
-                    tk.type = TokenType::LESS_OR_EQUAL;
+                    m_currToken.type = TokenType::LESS_OR_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::LESS_THAN;
+                    m_currToken.type = TokenType::LESS_THAN;
                 }
                 break;
             case '>':
                 if (*next == '=') {
-                    tk.type = TokenType::GREATER_OR_EQUAL;
+                    m_currToken.type = TokenType::GREATER_OR_EQUAL;
                     ++m_pos;
                 }
                 else {
-                    tk.type = TokenType::GREATER_THAN;
+                    m_currToken.type = TokenType::GREATER_THAN;
                 }
                 break;
             }
 
         }
         // get comment token
-        void Lexer::parseComment(Token& tk) const {
+        void Lexer::parseComment() const {
             if (*m_pos == '/' && *(m_pos + 1) == '/') {
-                tk.type = TokenType::COMMENT;
+                m_currToken.type = TokenType::COMMENT;
                 m_pos += 2; // skip these two slashes
                 while (*m_pos != '\n' && *m_pos != '\0') {
-                    tk.symbol += *m_pos++;
+                    m_currToken.symbol += *m_pos++;
                 }
                 --m_pos;
             }
             else { // ill-formed call to function
-                tk.type = TokenType::INVALID;
+                m_currToken.type = TokenType::INVALID;
             }
         }
 
@@ -291,6 +292,14 @@ namespace Simplexer {
         {
             setString(string);
         }
+
+        // iterator (wrapper for next())
+        struct Iterator {
+            using iterator_category = std::input_iterator_tag;
+            using value_type = Token;
+            using pointer = Token*;
+            using reference = Token&;
+        };
 
         // read file content
         // return true on success
@@ -323,40 +332,39 @@ namespace Simplexer {
         }
 
         // return Token structure from current index
-        const Token Lexer::next() const {
-            Token tk;
+        Token Lexer::next() const {
 
             // skip spaces
             while (std::isspace(*m_pos)) { ++m_pos; }
 
             // set symbol line
-            getSymbolLine(tk);
+            getSymbolLine();
 
             if (std::isalpha(*m_pos) || *m_pos == '_') { // symbol if encounted char or underscore
-                parseSymbol(tk);
+                parseSymbol();
             }
             else if (*m_pos == '\'' || *m_pos == '"') { // string
-                parseString(tk);
+                parseString();
             }
             else if (std::isdigit(*m_pos)) { // integer or rational
-                parseNumber(tk);
+                parseNumber();
             }
             else if (*m_pos == '.') {
                 if (isdigit(*(m_pos + 1))) { // if char after current position is a digit
-                    parseNumber(tk); // parse number
+                    parseNumber(); // parse number
                 }
                 else { // else assume as a standalone point
-                    tk.type = TokenType::POINT;
+                    m_currToken.type = TokenType::POINT;
                 }
             }
             else if (*m_pos == '\0') {
-                tk.type = TokenType::END_OF_FILE;
+                m_currToken.type = TokenType::END_OF_FILE;
             }
             else {
                 switch (*m_pos) {
                 case '/': // can be a comment
                     if (*(m_pos + 1) == '/') { // handle as a comment
-                        parseComment(tk);
+                        parseComment();
                         break;
                     }
                     [[fallthrough]]; // fall below if not a comment
@@ -366,30 +374,30 @@ namespace Simplexer {
                 case '=':
                 case '<':
                 case '>':
-                    parseOperator(tk);
+                    parseOperator();
                     break;
-                case ',': tk.type = TokenType::COMMA; break;
-                case ';': tk.type = TokenType::SEMICOLON; break;
-                case ':': tk.type = TokenType::COLON; break;
-                case '(': tk.type = TokenType::LEFT_PARAN; break;
-                case ')': tk.type = TokenType::RIGHT_PARAN; break;
-                case '[': tk.type = TokenType::LEFT_SQUARE; break;
-                case ']': tk.type = TokenType::RIGHT_SQUARE; break;
-                case '{': tk.type = TokenType::LEFT_CURLY; break;
-                case '}': tk.type = TokenType::RIGHT_CURLY; break;
+                case ',': m_currToken.type = TokenType::COMMA; break;
+                case ';': m_currToken.type = TokenType::SEMICOLON; break;
+                case ':': m_currToken.type = TokenType::COLON; break;
+                case '(': m_currToken.type = TokenType::LEFT_PARAN; break;
+                case ')': m_currToken.type = TokenType::RIGHT_PARAN; break;
+                case '[': m_currToken.type = TokenType::LEFT_SQUARE; break;
+                case ']': m_currToken.type = TokenType::RIGHT_SQUARE; break;
+                case '{': m_currToken.type = TokenType::LEFT_CURLY; break;
+                case '}': m_currToken.type = TokenType::RIGHT_CURLY; break;
                     // TODO: other cases
                 default:
-                    tk.type = TokenType::INVALID;
+                    m_currToken.type = TokenType::INVALID;
                 }
             }
 
             // always return EOF when m_pos is on the last character
-            if (tk.type != TokenType::END_OF_FILE) {
+            if (m_currToken.type != TokenType::END_OF_FILE) {
                 // as far as any parsing process stops on last character
                 ++m_pos; // move pointer forward
             }
 
-            return tk;
+            return m_currToken;
         }
     };
 }
